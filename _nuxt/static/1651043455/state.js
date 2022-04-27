9b4ecb71bf5c358f6c645894148fd8d51312c188
MertvyeDushi <MertvyeDushi@users.noreply.github.com>
2022-04-27 07:10:56 +0000

    deploy: d5346b4f8d0b8bb8bc5fd7e1804afd234b6f09d7

---
+++ _nuxt/static/1651043455/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmedia-cube--todo\u002F_nuxt\u002Fstatic\u002F1651043455",layout:"default",error:a,state:{form:{editableTaskId:a},tasks:{tasks:[],activeType:"all"}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmedia-cube--todo\u002F",assetsPath:"\u002Fmedia-cube--todo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
